fac1c27cedb7e2763ebc28a6d1a8481d5b07218a
Arjan <357noob@gmail.com>
2021-09-27 20:26:32 +0100

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button unitConversionBtn;
+    private Button equationsButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        unitConversionBtn = findViewById(R.id.unitConversionButton);
+        equationsButton = findViewById(R.id.equationsButton);
+        unitConversionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, UnitConversion.class);
+                startActivity(intent);
+            }
+        });
+        equationsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Equations.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
