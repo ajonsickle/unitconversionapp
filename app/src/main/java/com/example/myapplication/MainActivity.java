b92210ab0871e9f2137f86c8d30cba0209c3247e
Arjan <357noob@gmail.com>
2021-09-29 22:13:47 +0100

    calculator basic design created

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,13 +12,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button unitConversionBtn;
     private Button equationsButton;
+    private Button calculatorButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        calculatorButton = findViewById(R.id.calculatorButton);
         unitConversionBtn = findViewById(R.id.unitConversionButton);
         equationsButton = findViewById(R.id.equationsButton);
         unitConversionBtn.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Equations.class);
+                startActivity(intent);
+            }
+        });
+        calculatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Calculator.class);
                 startActivity(intent);
             }
         });
