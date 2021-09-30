15e8b7ff7d0815c2665f05053127559829df3a9b
Arjan <357noob@gmail.com>
2021-09-30 22:00:04 +0100

    improved calculator design, began functionality

--- app/src/main/java/com/example/myapplication/Calculator.java
+++ app/src/main/java/com/example/myapplication/Calculator.java
@@ -2,14 +2,186 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Calculator extends Activity {
+    boolean operatorClicked = false;
+    boolean decimalClicked = false;
+    String inputString = "";
+    double currentValue = 0;
+    List<Double> inputs = new ArrayList<Double>();
+    double previousValue;
+    private TextView previousEquation;
+    private TextView editTextTextPersonName;
+    private Button equalsButton, decimalButton, button21, button24, button25, button26, button27, button28, button29, button30, button31, button32, button33, button34, button35, button36, button37, button38, button39, button40;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+        previousEquation = findViewById(R.id.previousEquation);
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        equalsButton = findViewById(R.id.equalsButton);
+        decimalButton = findViewById(R.id.decimalButton);
+        button21 = findViewById(R.id.button21);
+        button24 = findViewById(R.id.button24);
+        button25 = findViewById(R.id.button25);
+        button26 = findViewById(R.id.button26);
+        button27 = findViewById(R.id.button27);
+        button28 = findViewById(R.id.button28);
+        button29 = findViewById(R.id.button29);
+        button30 = findViewById(R.id.button30);
+        button31 = findViewById(R.id.button31);
+        button32 = findViewById(R.id.button32);
+        button33 = findViewById(R.id.button33);
+        button34 = findViewById(R.id.button34);
+        button35 = findViewById(R.id.button35);
+        button36 = findViewById(R.id.button36);
+        button37 = findViewById(R.id.button37);
+        button38 = findViewById(R.id.button38);
+        button39 = findViewById(R.id.button39);
+        button40 = findViewById(R.id.button40);
 
 
+        equalsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        decimalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button21.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button25.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (operatorClicked) {
+                    operatorClicked = false;
+                    inputString += "0";
+                    currentValue = 0;
+                    editTextTextPersonName.setText(inputString);
+                } else {
+                    inputString += "0";
+                    String currentVal = String.valueOf(currentValue);
+                    currentVal += "0";
+                    currentValue = Double.parseDouble(currentVal);
+                    editTextTextPersonName.setText(inputString);
+                }
+            }
+        });
+        button26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button27.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button28.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button29.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button30.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button31.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button33.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button34.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button35.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button36.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button37.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button38.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button39.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        button40.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
